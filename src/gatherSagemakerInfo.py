4dcfd0edeaa632fd0276bb6e4d4645916836af3d
Alex Chirayath <alexchir@amazon.com>
2020-05-29 16:03:46 -0700

    Adding Sagemaker and Recuurrence support

---
+++ src/gatherSagemakerInfo.py
@@ -0,0 +1,76 @@
+import json
+import boto3
+import os
+from helper import getEC2Regions, sendDataToSNS, OPTOUT_TAG, SNS_NOTIFICATION_IIAS_SAGEMAKER
+
+
+def getSageMakerSupportedRegions():
+    ssmClient = boto3.client('ssm')
+    
+    response =ssmClient.get_parameters_by_path(
+        Path = '/aws/service/global-infrastructure/services/sagemaker/regions'
+        )
+    sagemakerRegionList = [regionInfo['Value'] for regionInfo in response['Parameters']]
+    
+    ec2RegionList = getEC2Regions() # getRegions that are enabled
+    return list(set(sagemakerRegionList) & set(ec2RegionList))
+     
+
+
+def getSageMakerFilteredRegionalInstanceInfo(region):
+    sagemakerRegionalClient = boto3.client('sagemaker', region_name = region)
+    paginator = sagemakerRegionalClient.get_paginator('list_notebook_instances')
+    page_iterator = paginator.paginate()
+    allSagemakerInstances = []
+    for result in page_iterator:
+        for instance in result['NotebookInstances']:
+            if instance.get('NotebookInstanceLifecycleConfigName') is None: #Only include instances with no lifecycle config
+                allSagemakerInstances.append({'NotebookInstanceArn': instance['NotebookInstanceArn'] , 'Tags': getSagemakerInstanceTags(sagemakerRegionalClient,instance['NotebookInstanceArn']), 'NotebookInstanceStatus':instance['NotebookInstanceStatus'], 'NotebookInstanceName':instance['NotebookInstanceName'] })
+    return excludeOptedOutSagemakerInstances(sagemakerRegionalClient,allSagemakerInstances)    
+
+def getSagemakerInstanceTags(sagemakerRegionalClient,instanceArn):
+    return sagemakerRegionalClient.list_tags(ResourceArn= instanceArn)['Tags']
+    
+
+def excludeOptedOutSagemakerInstances(sagemakerRegionalClient,sagemakerInstances):
+    filteredSagemakerInstancesList = []
+    for instanceInfo in sagemakerInstances:
+        if isExcludedSagemakerInstance(instanceInfo):
+            print('Exlcuding instance {}'.format(instanceInfo))
+        else:
+            if instanceInfo['NotebookInstanceStatus']== 'InService':
+                sagemakerRegionalClient.stop_notebook_instance(NotebookInstanceName=instanceInfo['NotebookInstanceName'])
+            filteredSagemakerInstancesList.append(instanceInfo['NotebookInstanceArn'])
+    return filteredSagemakerInstancesList
+
+def isExcludedSagemakerInstance(instanceInfo): 
+    if any((d['Key'] == 'IIASOptOut' and d['Value'] == 'True')for d in instanceInfo['Tags']):
+        return True
+
+def getSageMakerInstances():
+    regionList = getSageMakerSupportedRegions()
+    sagemakerRegionDict = {}
+    for region in regionList:
+        regionalInstances = getSageMakerFilteredRegionalInstanceInfo(region)
+        if len(regionalInstances)>0:
+            sagemakerRegionDict[region]=regionalInstances
+    return sagemakerRegionDict
+
+def handler(event, context):
+    sagemakerRegionalInfo = getSageMakerInstances()
+    if len(sagemakerRegionalInfo.keys())!=0:
+        print('Sending following sagemaker info for Lifecycle config : {}'.format(sagemakerRegionalInfo))
+        messageAttributes = {
+            'notificationFor': {
+                'DataType': 'String',
+                'StringValue': SNS_NOTIFICATION_IIAS_SAGEMAKER
+            }
+        }
+        sendDataToSNS(sagemakerRegionalInfo,messageAttributes)
+    else:
+        print('No new notebook instances in IIAS scope')
+        
+        
+
+
+
---
+++ src/updateSagemakerInstances.py
@@ -0,0 +1,73 @@
+import json
+import boto3
+import re
+import base64
+from helper import  sendDataToSNS, SNS_NOTIFICATION_IIAS_EMAIL
+
+
+SAGEMAKER_INSTANCE_ARN_REGEX = '(arn:(aws|aws-cn))(:sagemaker:)([\w\d\-]+):([0-9]+):notebook-instance\/(.+)'
+IIAS_CONFIG_NAME = 'IIAS-Sagemaker-Idle-Auto-Stop-Config'
+sagemakerInstancesActedOn = list()
+
+
+
+def isIIASLifeCycleConfigPresent(region):
+    sagemakerRegionalClient=boto3.client('sagemaker',region_name=region)
+    response =sagemakerRegionalClient.list_notebook_instance_lifecycle_configs(
+    NameContains=IIAS_CONFIG_NAME
+    )
+    return len(response['NotebookInstanceLifecycleConfigs'])>0
+    
+def createIIASLifeCycleConfig(region):
+    sagemakerRegionalClient=boto3.client('sagemaker',region_name=region)
+    lifecycleConfigContentFile = open("./static/sm-on-start.sh", "r")
+    lifecycleConfigContent = lifecycleConfigContentFile.read()
+    sagemakerRegionalClient.create_notebook_instance_lifecycle_config(
+        NotebookInstanceLifecycleConfigName=IIAS_CONFIG_NAME,
+        OnStart=[
+            {
+                'Content': base64.b64encode(lifecycleConfigContent.encode('utf-8')).decode('utf-8')
+            }
+        ]
+    )
+
+def getInstanceNamefromArn(instanceArn):
+    captured_groups = re.search(SAGEMAKER_INSTANCE_ARN_REGEX, instanceArn)
+    return captured_groups.group(6)
+
+def updateNotebookLifecycleConfigAndTags(region, sagemakerInstanceList):
+    sagemakerRegionalClient=boto3.client('sagemaker',region_name=region)
+    for instanceArn in sagemakerInstanceList:
+        sagemakerRegionalClient.update_notebook_instance(
+            NotebookInstanceName=getInstanceNamefromArn(instanceArn),
+            LifecycleConfigName=IIAS_CONFIG_NAME
+        )
+        sagemakerInstancesActedOn.append(instanceArn)
+
+def updateRegionalSagemakerLifeCycleConfigs(region, sagemakerInstanceList):
+    if not isIIASLifeCycleConfigPresent(region):
+        createIIASLifeCycleConfig(region)  
+    updateNotebookLifecycleConfigAndTags(region, sagemakerInstanceList)
+    
+    
+
+def updateSagemakerLifecycleConfigs(sagemakerRegionalInfo):
+    for region in sagemakerRegionalInfo.keys():
+        updateRegionalSagemakerLifeCycleConfigs(region, sagemakerRegionalInfo[region] )
+
+def handler(event, context):
+    sagemakerRegionalInfo =json.loads(event['Records'][0]['Sns']['Message'])
+    print ('Received sagemakerRegionalInfo : {}'.format(sagemakerRegionalInfo))
+    updateSagemakerLifecycleConfigs(sagemakerRegionalInfo)
+    if len(sagemakerInstancesActedOn)!=0:
+        messageAttributes = {
+            'notificationFor': {
+                'DataType': 'String',
+                'StringValue': SNS_NOTIFICATION_IIAS_EMAIL
+            }
+        }
+        sendDataToSNS({
+            'Description' :  'IIAS has stopped and applied idle check alarms for the following Sagemaker notebook instances:' ,
+            'instanceList' : sagemakerInstancesActedOn
+            },
+        messageAttributes)
